a72b14dbd1f27b498c28ebcf96981bc42a6582a0
程宏 <chenghong.1020@bytedance.com>
2025-02-11 22:15:12 +0400

    refactor game utilitis

---
+++ app/scripts/core/gameUtil.js
@@ -0,0 +1,162 @@
+class GameUtilities {
+  /**
+   * Reference to the GameCoordinator instance.
+   * @type {Object}
+   */
+  constructor(gameCoord) {
+    this.gameCoord = gameCoord;
+  }
+
+  /**
+   * Load all assets into a hidden Div to pre-load them into memory.
+   * There is probably a better way to read all of these file names.
+   */
+  preloadAssets() {
+    return new Promise((resolve) => {
+      const loadingContainer = document.getElementById('loading-container');
+      const loadingPacman = document.getElementById('loading-pacman');
+      const loadingDotMask = document.getElementById('loading-dot-mask');
+
+      const imgBase = 'app/style/graphics/spriteSheets/';
+      const imgSources = [
+        // Pacman
+        `${imgBase}characters/pacman/arrow_down.svg`,
+        `${imgBase}characters/pacman/arrow_left.svg`,
+        `${imgBase}characters/pacman/arrow_right.svg`,
+        `${imgBase}characters/pacman/arrow_up.svg`,
+        `${imgBase}characters/pacman/pacman_death.svg`,
+        `${imgBase}characters/pacman/pacman_error.svg`,
+        `${imgBase}characters/pacman/pacman_down.svg`,
+        `${imgBase}characters/pacman/pacman_left.svg`,
+        `${imgBase}characters/pacman/pacman_right.svg`,
+        `${imgBase}characters/pacman/pacman_up.svg`,
+
+        // Blinky
+        `${imgBase}characters/ghosts/blinky/blinky_down_angry.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_down_annoyed.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_down.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_left_angry.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_left_annoyed.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_left.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_right_angry.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_right_annoyed.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_right.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_up_angry.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_up_annoyed.svg`,
+        `${imgBase}characters/ghosts/blinky/blinky_up.svg`,
+
+        // Clyde
+        `${imgBase}characters/ghosts/clyde/clyde_down.svg`,
+        `${imgBase}characters/ghosts/clyde/clyde_left.svg`,
+        `${imgBase}characters/ghosts/clyde/clyde_right.svg`,
+        `${imgBase}characters/ghosts/clyde/clyde_up.svg`,
+
+        // Inky
+        `${imgBase}characters/ghosts/inky/inky_down.svg`,
+        `${imgBase}characters/ghosts/inky/inky_left.svg`,
+        `${imgBase}characters/ghosts/inky/inky_right.svg`,
+        `${imgBase}characters/ghosts/inky/inky_up.svg`,
+
+        // Pinky
+        `${imgBase}characters/ghosts/pinky/pinky_down.svg`,
+        `${imgBase}characters/ghosts/pinky/pinky_left.svg`,
+        `${imgBase}characters/ghosts/pinky/pinky_right.svg`,
+        `${imgBase}characters/ghosts/pinky/pinky_up.svg`,
+
+        // Ghosts Common
+        `${imgBase}characters/ghosts/eyes_down.svg`,
+        `${imgBase}characters/ghosts/eyes_left.svg`,
+        `${imgBase}characters/ghosts/eyes_right.svg`,
+        `${imgBase}characters/ghosts/eyes_up.svg`,
+        `${imgBase}characters/ghosts/scared_blue.svg`,
+        `${imgBase}characters/ghosts/scared_white.svg`,
+
+        // Dots
+        `${imgBase}pickups/pacdot.svg`,
+        `${imgBase}pickups/powerPellet.svg`,
+
+        // Fruit
+        `${imgBase}pickups/apple.svg`,
+        `${imgBase}pickups/bell.svg`,
+        `${imgBase}pickups/cherry.svg`,
+        `${imgBase}pickups/galaxian.svg`,
+        `${imgBase}pickups/key.svg`,
+        `${imgBase}pickups/melon.svg`,
+        `${imgBase}pickups/orange.svg`,
+        `${imgBase}pickups/strawberry.svg`,
+
+        // Text
+        `${imgBase}text/ready.svg`,
+
+        // Points
+        `${imgBase}text/100.svg`,
+        `${imgBase}text/200.svg`,
+        `${imgBase}text/300.svg`,
+        `${imgBase}text/400.svg`,
+        `${imgBase}text/500.svg`,
+        `${imgBase}text/700.svg`,
+        `${imgBase}text/800.svg`,
+        `${imgBase}text/1000.svg`,
+        `${imgBase}text/1600.svg`,
+        `${imgBase}text/2000.svg`,
+        `${imgBase}text/3000.svg`,
+        `${imgBase}text/5000.svg`,
+
+        // Maze
+        `${imgBase}maze/maze_blue.svg`,
+
+        // Misc
+        'app/style/graphics/extra_life.svg',
+      ];
+
+      const audioBase = 'app/style/audio/';
+      const audioSources = [
+        `${audioBase}game_start.mp3`,
+        `${audioBase}pause.mp3`,
+        `${audioBase}pause_beat.mp3`,
+        `${audioBase}siren_1.mp3`,
+        `${audioBase}siren_2.mp3`,
+        `${audioBase}siren_3.mp3`,
+        `${audioBase}power_up.mp3`,
+        `${audioBase}extra_life.mp3`,
+        `${audioBase}eyes.mp3`,
+        `${audioBase}eat_ghost.mp3`,
+        `${audioBase}death.mp3`,
+        `${audioBase}fruit.mp3`,
+        `${audioBase}dot_1.mp3`,
+        `${audioBase}dot_2.mp3`,
+      ];
+
+      const totalSources = imgSources.length + audioSources.length;
+      this.gameCoord.remainingSources = totalSources;
+
+      loadingPacman.style.left = '0';
+      loadingDotMask.style.width = '0';
+
+      Promise.all([
+        this.gameCoord.createElements(imgSources, 'img', totalSources, this),
+        this.gameCoord.createElements(
+          audioSources,
+          'audio',
+          totalSources,
+          this,
+        ),
+      ])
+        .then(() => {
+          loadingContainer.style.opacity = 0;
+          resolve();
+
+          setTimeout(() => {
+            loadingContainer.remove();
+            this.gameCoord.mainMenu.style.opacity = 1;
+            this.gameCoord.mainMenu.style.visibility = 'visible';
+          }, 1500);
+        })
+        .catch(this.gameCoord.displayErrorMessage);
+    });
+  }
+}
+
+// removeIf(production)
+module.exports = GameUtilities;
+// endRemoveIf(production)
